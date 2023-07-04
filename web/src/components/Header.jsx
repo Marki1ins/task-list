412acbce7705e70396ac4f5bd52d263ddd84af12
Marki1ins <marcosdfcarvalhoo@gmail.com>
2023-07-04 13:42:08 -0300

    feat: Adding route "post" to create new tasks

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,12 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function Header() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      if (inputValue.trim() !== "") {
+        await axios.post("http://localhost:3003/task", { title: inputValue });
+        setInputValue("");
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
   return (
     <header className="w-full pt-4">
       <h1 className="mb-4 text-4xl text-grey font-light">Task List</h1>
-      <form className="flex items-center justify-between w-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-between w-full"
+      >
         <input
           type="text"
+          value={inputValue}
+          onChange={handleChange}
           placeholder="What do you have planned?"
           className="flex-1 py-3 px-4 bg-dark-800 rounded-2xl text-xl text-light outline-none placeholder:text-grey"
+          required
         />
         <button
           type="submit"
