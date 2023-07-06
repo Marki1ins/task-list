24224133ce7264fbb133ac248058542ddffc2dc4
Marki1ins <marcosdfcarvalhoo@gmail.com>
2023-07-05 22:42:42 -0300

    style: adding margin-top to component

--- web/src/components/NoTasks.jsx
+++ web/src/components/NoTasks.jsx
@@ -2,7 +2,7 @@ import { NoteBlank } from "@phosphor-icons/react";
 
 export function NoTasks() {
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col items-center w-full mt-44">
       <NoteBlank className="w-44 h-44 text-grey/30" />
       <h3 className="text-5xl text-grey/30 font-normal">No Tasks</h3>
     </div>
